496d4c873831de3c29be0851dbacb6b992432d5b
alicesaunders <alice.saunders01@gmail.com>
2026-05-06 11:03:07 +0100

    Add error message if a character is entered incorrectly

--- app.js
+++ app.js
@@ -2,7 +2,15 @@
 function lettersOnly(input){
     // define regular expression 
     var regex = /[^a-z|\s]/gi;
+    // store value from the input field
+    var original = input.value 
     // replace anything not defined in the regex with ""
     input.value = input.value.replace(regex, "");
-
+    // check if anything was removed
+    if (original !== input.value) {
+        document.getElementById("error-message").textContent =
+            "Please only enter the ingredient name in full!";
+    } else {
+        document.getElementById("error-message").textContent = "";
+    }
 }
